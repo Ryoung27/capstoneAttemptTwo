5e8da1d6ce5e9d7908e1218d3415d3f3b7eb3f1b
Ryoung27 <RRYoung89@gmail.com>
2018-09-12 14:36:35 -0500

    fixed the break down of data

--- website/views/day_view.py
+++ website/views/day_view.py
@@ -20,10 +20,20 @@ def day_view(request, pk):
     #Currently converting Day to Day_User
     if request.method == "GET":
         day = get_object_or_404(Day, pk=pk)
-        thoughts = Day.objects.filter(id = day.id)[0].thoughts
-        time = Day.objects.filter(id = day.id)[0].time
-        Day_Form = DayForm()
-        return render(request, 'run/day.html', {"day":day, "thoughts": thoughts, "time": time, "Day_Form": Day_Form})
+
+        user_day= Day_User.objects.filter(day=day.id, user=request.user)
+        if user_day:
+            user_day= Day_User.objects.get(day=day.id, user=request.user)
+            # user_day[0]
+            time = Day_User.objects.filter(id = day.id)[0].time
+            Day_Form = DayForm()
+            print(request.user.id)
+            return render(request, 'run/day.html', {"day":day, "user_day": user_day, "time": time, "Day_Form": Day_Form})
+        else:
+            Day_Form = DayForm()
+            return render(request, "run/day.html", {"day":day, "Day_Form": Day_Form} )
+
+
     # Next hellzone. I need Kimmy or Joe's help with this.
     # elif request.method == "POST":
     #     data = Day_Join_Table(
@@ -42,5 +52,3 @@ def day_view(request, pk):
        return  HttpResponseRedirect(reverse('website:day_view', args=(pk)))
 
 
-
-#Change ERD completely.
